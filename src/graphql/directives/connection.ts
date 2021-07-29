c39ef5693abca592cf778f08c61aa0c08c1f8ac3
iamchanii <imch@dano.me>
2021-07-29 21:59:20 +0900

    feat: extend connection args on field

--- src/graphql/directives/connection.ts
+++ src/graphql/directives/connection.ts
@@ -5,24 +5,18 @@ import {
   mapSchema,
 } from '@graphql-tools/utils';
 import {
-  DirectiveNode,
   GraphQLBoolean,
-  GraphQLFieldConfig,
+  GraphQLInt,
   GraphQLList,
   GraphQLNamedType,
   GraphQLNonNull,
   GraphQLObjectType,
+  GraphQLOutputType,
   GraphQLSchema,
   GraphQLString,
 } from 'graphql';
 
 const DIRECTIVE_NAME = 'connection';
-
-const getConnectionTypeName = (fieldConfig: GraphQLFieldConfig<any, any>) =>
-  `${fieldConfig.type.toString()}Connection`;
-
-const getEdgeTypeName = (fieldConfig: GraphQLFieldConfig<any, any>) =>
-  `${fieldConfig.type.toString()}Edge`;
 
 export const connectionDirective = {
   typeDefs: `directive @${DIRECTIVE_NAME} on FIELD_DEFINITION`,
@@ -48,57 +42,56 @@ export const connectionDirective = {
       mapSchema(schema, {
         [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
           const directives = getDirectives(schema, fieldConfig);
-          const f = fieldConfig as any;
 
           if (directives[DIRECTIVE_NAME]) {
-            if (!knownTypeMap[getEdgeTypeName(fieldConfig)]) {
-              knownTypeMap[getEdgeTypeName(fieldConfig)] =
-                new GraphQLObjectType({
-                  name: getEdgeTypeName(fieldConfig),
-                  fields: {
-                    node: {
-                      type: GraphQLNonNull(
-                        knownTypeMap[fieldConfig.type.toString()]
-                      ),
-                    },
-                    cursor: { type: GraphQLNonNull(GraphQLString) },
+            const typeName = fieldConfig.type.toString();
+            const connectionTypeName = `${typeName}Connection`;
+            const edgeTypeName = `${typeName}Edge`;
+
+            if (!knownTypeMap[edgeTypeName]) {
+              knownTypeMap[edgeTypeName] = new GraphQLObjectType({
+                name: edgeTypeName,
+                fields: {
+                  node: {
+                    type: GraphQLNonNull(knownTypeMap[typeName]),
                   },
-                });
+                  cursor: { type: GraphQLNonNull(GraphQLString) },
+                },
+              });
 
-              typesToAdd.push(knownTypeMap[getEdgeTypeName(fieldConfig)]);
+              typesToAdd.push(knownTypeMap[edgeTypeName]);
             }
 
-            if (!knownTypeMap[getConnectionTypeName(fieldConfig)]) {
-              knownTypeMap[getConnectionTypeName(fieldConfig)] =
-                new GraphQLObjectType({
-                  name: getConnectionTypeName(fieldConfig),
-                  fields: {
-                    edges: {
-                      type: GraphQLNonNull(
-                        GraphQLList(
-                          GraphQLNonNull(
-                            knownTypeMap[getEdgeTypeName(fieldConfig)]
-                          )
-                        )
-                      ),
-                    },
-                    pageInfo: {
-                      type: GraphQLNonNull(knownTypeMap['PageInfo']),
-                    },
+            if (!knownTypeMap[connectionTypeName]) {
+              knownTypeMap[connectionTypeName] = new GraphQLObjectType({
+                name: connectionTypeName,
+                fields: {
+                  edges: {
+                    type: GraphQLNonNull(
+                      GraphQLList(GraphQLNonNull(knownTypeMap[edgeTypeName]))
+                    ),
                   },
-                });
+                  pageInfo: {
+                    type: GraphQLNonNull(knownTypeMap['PageInfo']),
+                  },
+                },
+              });
 
-              typesToAdd.push(knownTypeMap[getConnectionTypeName(fieldConfig)]);
+              typesToAdd.push(knownTypeMap[connectionTypeName]);
             }
 
-            const directiveIndex = fieldConfig.astNode?.directives?.findIndex(
-              (directive: DirectiveNode) =>
-                directive.name.value !== DIRECTIVE_NAME
-            );
+            fieldConfig.type = knownTypeMap[
+              connectionTypeName
+            ] as GraphQLOutputType;
 
-            if (directiveIndex && directiveIndex >= 0) {
-              f.astNode.directives.splice(directiveIndex, 1);
+            if (!fieldConfig.args) {
+              fieldConfig.args = {};
             }
+
+            fieldConfig.args.first = { type: GraphQLInt };
+            fieldConfig.args.after = { type: GraphQLString };
+            fieldConfig.args.last = { type: GraphQLInt };
+            fieldConfig.args.before = { type: GraphQLString };
           }
 
           return fieldConfig;
