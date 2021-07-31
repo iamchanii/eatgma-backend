9fd8d8b49cf851cc7ba0b331f29fa790157e38b8
iamchanii <imch@dano.me>
2021-07-31 17:30:36 +0900

    feat: write schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,8 +1,10 @@
+import { writeFileSync } from 'fs';
 import { join } from 'path';
 import { connectionDirective } from './directives/connection';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import { printSchema } from 'graphql';
 
 export const schema = makeExecutableSchema({
   typeDefs: [
@@ -14,4 +16,17 @@ export const schema = makeExecutableSchema({
     ),
   ],
   schemaTransforms: [connectionDirective.transformer],
+  resolvers: {
+    Query: {
+      ping: (_, __, context) => {
+        console.log(context.orm);
+        return 'Pong!';
+      },
+    },
+  },
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  const schemaPath = join(process.cwd(), 'schema.graphql');
+  writeFileSync(schemaPath, printSchema(schema), { encoding: 'utf8' });
+}
