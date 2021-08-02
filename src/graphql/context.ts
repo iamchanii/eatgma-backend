c3c4ab12a47adb43b6d21bb38faadac1e51b7486
iamchanii <imch@dano.me>
2021-08-02 23:52:25 +0900

    feat: 액세스 토큰을 사용해서 컨텍스트에 유저 정보 저장

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,12 +1,27 @@
 import { getMikroInstance } from '../db';
+import { verifyToken } from '../models/User/lib/jwtUtils';
+import { IUser } from './__generated';
 import type { MikroORM } from '@mikro-orm/core';
+import { Request } from 'graphql-helix';
 
 export interface OwnContext {
   orm: MikroORM;
+  user: IUser | null;
 }
 
-export const createContext = async (): Promise<OwnContext> => {
+export const createContext = async (ctx: {
+  request: Request;
+}): Promise<OwnContext> => {
+  let user: OwnContext['user'] = null;
+  const authorization: string = (ctx.request.headers as any)['authorization'];
+
+  if (authorization) {
+    const accessToken = authorization.split('Bearer ')[1];
+    user = verifyToken(accessToken).payload as IUser;
+  }
+
   return {
     orm: await getMikroInstance(),
+    user,
   };
 };
--- src/graphql/envelop.ts
+++ src/graphql/envelop.ts
@@ -2,11 +2,10 @@ import { createContext } from './context';
 import { schema } from './schema';
 import {
   envelop,
+  Plugin,
   useExtendContext,
-  useLogger,
   useMaskedErrors,
   useSchema,
-  Plugin,
 } from '@envelop/core';
 import { getGraphQLParameters, processRequest, Request } from 'graphql-helix';
 
@@ -14,7 +13,6 @@ const getEnveloped = envelop({
   plugins: [
     useSchema(schema),
     useExtendContext(createContext),
-    useLogger(),
     process.env.NODE_ENV === 'production' && useMaskedErrors(),
   ].filter(Boolean) as Plugin[],
 });
