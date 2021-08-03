fb421cba54af5f48c42648b3d4229fea0c410249
iamchanii <imch@dano.me>
2021-08-03 22:15:06 +0900

    feat: 컨텍스트 생성 중 에러 발생 시 적절한 메시지 표시

--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,8 +1,10 @@
 import { getMikroInstance } from '../db';
 import { verifyToken } from '../models/User/lib/jwtUtils';
 import { IUser } from './__generated';
+import { EnvelopError } from '@envelop/core';
 import type { MikroORM } from '@mikro-orm/core';
 import { Request } from 'graphql-helix';
+import { TokenExpiredError } from 'jsonwebtoken';
 
 export interface OwnContext {
   orm: MikroORM;
@@ -17,7 +19,16 @@ export const createContext = async (ctx: {
 
   if (authorization) {
     const accessToken = authorization.split('Bearer ')[1];
-    user = verifyToken(accessToken).payload as IUser;
+
+    try {
+      user = verifyToken(accessToken).payload as IUser;
+    } catch (e) {
+      if (e.name === TokenExpiredError.name) {
+        throw new EnvelopError('액세스 토큰이 만료되었습니다.');
+      } else {
+        throw new Error('예기치 않은 오류가 발생했습니다.');
+      }
+    }
   }
 
   return {
